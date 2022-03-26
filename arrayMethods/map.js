6beb2222327c11a03db2cd2b0d413035c821aca8
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-03-26 16:59:41 +0300

    update map

--- arrayMethods/map.js
+++ arrayMethods/map.js
@@ -1,3 +1,4 @@
+// Array:
 const people = [
    { id: 1, name: 'Evgeniy', age: 22, isOnline: true },
    { id: '2', name: 'Igor', age: 25, isOnline: true },
@@ -6,28 +7,19 @@ const people = [
    { id: 5, name: 'Oleg', age: 41, isOnline: false },
 ]
 
-// // Преобразовать все значения в числа
+// // Преобразовать все id в числа, поменять всем isOnline на false, добавить всем фамилии и всем прибавить + 10 к возрасту
 // console.log(people.map(p => {
-//    return { ...p, id: +p.id }
+//    return { ...p, id: +p.id, isOnline: false, surName: 'Ivanov', age: p.age + 10 }
 // }))
 
-// // Изменить всем isOnline на false и всем прибавить + 10 к возрасту
-// console.log(people.map(p => {
-//    return { ...p, isOnline: false, age: p.age += 10 }
-// }))
-
-// // Изменить isOnline на true конкретному человеку
+// // Поменять isOnline на true конкретному объекту, который пройдет определенное условие
 // console.log(people.map(p => p.id === 3
 //    ? { ...p, isOnline: true }
 //    : p
 // ))
 
-// // Добавить всем фамилию
-// console.log(people.map(p => {
-//    return { ...p, surName: 'Ivanov' }
-// }))
 
-// associativeArray
+// AssociativeArray:
 const todolistID_1 = 1
 const todolistID_2 = 2
 
@@ -39,14 +31,21 @@ const tasks = {
    ],
    [todolistID_2]: [
       { id: 1, title: 'Book', isDone: true },
-      { id: 2, title: 'Milk', isDone: false },
+      { id: '2', title: 'Milk', isDone: false },
    ],
 }
 
-// Изменить isDone на true конкретной таски
-console.log({
-   ...tasks, [todolistID_2]: tasks[todolistID_2].map(t => t.id === 2
-      ? { ...t, isDone: true }
-      : t
-   )
-})
+// // Преобразовать все id в числа, поменять всем isOnline на false и добавить всем новое свойство filter: 'all'
+// console.log({
+//    ...tasks, [todolistID_2]: tasks[todolistID_2].map(t => {
+//       return { id: +t.id, isDone: false, filter: 'all' }
+//    })
+// })
+
+// // Поменять isOnline на true конкретному объекту, который пройдет определенное условие
+// console.log({
+//    ...tasks, [todolistID_2]: tasks[todolistID_2].map(t => t.id === 2
+//       ? { ...t, isDone: true }
+//       : t
+//    )
+// })
