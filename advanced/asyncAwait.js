fe2402e5e7da0e03ace1e1966eb7acb55a0cd1fa
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-05-03 20:11:16 +0300

    updated async await

--- advanced/asyncAwait.js
+++ advanced/asyncAwait.js
@@ -1,5 +1,5 @@
 const asyncAwait = async () => {
-   try {
+   try { // Попытка выполнить какой-то код
       const res = await fetch('https://jsonplaceholder.typicode.com/users')
       const data = await res.json()
       const user = await data[0]
@@ -7,9 +7,9 @@ const asyncAwait = async () => {
       const geo = await address.geo
       const lat = await geo.lat
       console.log(lat)
-   } catch (err) {
+   } catch (err) { // Если внутри блока try возникает ошибка, то выполнится catch
       console.warn(err)
-   } finally {
+   } finally { // Выполнится в любом случае
       console.log('Finally')
    }
 }
