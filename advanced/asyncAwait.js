107af89e189ffbf5107884544d3215de126603a7
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-05-17 17:53:43 +0300

    update

--- advanced/asyncAwait.js
+++ advanced/asyncAwait.js
@@ -7,6 +7,14 @@ const asyncAwait = async () => {
       const geo = await address.geo
       const lat = await geo.lat
       console.log(lat)
+
+      const all = await Promise.all([data, user, address, geo, lat])
+      console.log(all)
+      const allSettled = await Promise.allSettled([data, user, address, geo, lat])
+      console.log(allSettled)
+      const race = await Promise.race([data, user, address, geo, lat])
+      console.log(race)
+
    } catch (err) { // Если внутри блока try возникает ошибка, то выполнится catch
       console.warn(err)
    } finally { // Выполнится в любом случае
@@ -15,3 +23,17 @@ const asyncAwait = async () => {
 }
 
 asyncAwait()
+
+// const asyncAwait2 = async () => {
+//    const resUsers = await fetch('https://jsonplaceholder.typicode.com/users')
+//    const dataUsers = await resUsers.json()
+//    console.log(dataUsers)
+//    const resToDos = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//    const dataToDos = await resToDos.json()
+//    console.log(dataToDos)
+//    const resPosts = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+//    const dataPosts = await resPosts.json()
+//    console.log(dataPosts)
+// }
+
+// asyncAwait2()
--- advanced/promise.js
+++ advanced/promise.js
@@ -1,47 +1,47 @@
 const promise = new Promise((resolve, reject) => {
-   fetch('https://jsonplaceholder.typicode.com/users')
-      // Promise chain
-      .then((res) => res.json())
-      .then((data) => data[0])
-      .then((user) => user.address)
-      .then((address) => address.geo)
-      .then((geo) => resolve(geo.lat))
-      .catch((err) => reject(err))
+  fetch('https://jsonplaceholder.typicode.com/users')
+    // Promise chain
+    .then((res) => res.json())
+    .then((data) => data[0])
+    .then((user) => user.address)
+    .then((address) => address.geo)
+    .then((geo) => resolve(geo.lat))
+    .catch((err) => reject(err))
 })
 
 promise
-   .then((lat) => console.log(lat))
-   .catch((err) => console.warn(err))
-   .finally(() => console.log('Finally'))
+  .then((lat) => console.log(lat))
+  .catch((err) => console.warn(err))
+  .finally(() => console.log('Finally'))
 
 const promise2 = new Promise((resolve, reject) => {
-   fetch('https://jsonplaceholder.typicode.com/users')
-      // Promise chain
-      .then((res) => res.json())
-      .then((data) => data[0])
-      .then((user) => user.address)
-      .then((address) => address.geo)
-      .then((geo) => resolve(geo.lng))
-      .catch((err) => reject(err))
+  fetch('https://jsonplaceholder.typicode.com/users')
+    // Promise chain
+    .then((res) => res.json())
+    .then((data) => data[0])
+    .then((user) => user.address)
+    .then((address) => address.geo)
+    .then((geo) => resolve(geo.lng))
+    .catch((err) => reject(err))
 })
 
 promise2
-   .then((lng) => console.log(lng))
-   .catch((err) => console.warn(err))
-   .finally(() => console.log('Finally'))
+  .then((lng) => console.log(lng))
+  .catch((err) => console.warn(err))
+  .finally(() => console.log('Finally'))
 
 Promise.all([promise, promise2])
-   .then((results) => console.log('Promise all:', results))
-   .catch((err) => console.warn(err))
+  .then((results) => console.log('Promise all:', results))
+  .catch((err) => console.warn(err))
 
 
 Promise.allSettled([promise, promise2])
-   .then((results) => console.log('allSettled:', results))
-   .catch((err) => console.warn(err))
+  .then((results) => console.log('allSettled:', results))
+  .catch((err) => console.warn(err))
 
 Promise.race([promise, promise2])
-   .then((result) => console.log('Promise race:', result))
-   .catch((err) => console.warn(err))
+  .then((result) => console.log('Promise race:', result))
+  .catch((err) => console.warn(err))
 
 // // Promise у которого изначально состояние 'fulfilled'
 // const resolvedPromise = Promise.resolve({ data: {}, status: 200 })
@@ -62,3 +62,20 @@ Promise.race([promise, promise2])
 //       console.log(rejectedPromise) // 'rejected'
 //       console.log(error)
 //    })
+
+// const p3 = () => {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data)
+//       return fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data)
+//       return fetch('https://jsonplaceholder.typicode.com/posts/1')
+//     })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+// }
+// p3()
