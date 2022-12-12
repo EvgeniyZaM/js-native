e2cc80c61054f9d7d8cd23a72b156cb6526fbae3
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-12-12 17:34:11 +0300

    commit

--- index.js
+++ index.js
@@ -1,24 +1,110 @@
-// setTimeout - вызывает callback функцию один раз, через определённое количество времени и возвращает id
-// setInterval - вызывает callback функцию каждый раз, через определенное количество времени и возвращает id
+"use strict"
 
-const timerId = setTimeout((value1, value2, value3) => {
-  console.log(`${value1} ${value2} ${value3}`)
-}, 1000, "1", "2", "3")
+// Прежде чем скрипт начнет выполняться, движок пробегается по коду 2 раза
 
-// Отмена вызова setTimeout
-clearTimeout(timerId)
+// 1 пробег:
+// Создается лексическое окружение, в котором создается ссылка на внешнее лексическое окружение и пустая окружающая среда
 
-const timerId2 = setInterval((value1, value2, value3) => {
-  console.log(`${value1} ${value2} ${value3}`)
-}, 1000, "1", "2", "3")
+// Движок собирает все объявленные переменные с помощью ключевого слова var и Function Declaration, поднимает их и добавляет в лексическое окружение (hoisting)
+// Function Declaration это будет ссылка на функцию, а в переменных объявленных с помощью ключевого слова var будет undefined.
+// Т.к. поднимается именно объявление переменной, а присвоение значения происходит там, где мы его явно присваиваем
 
-// Отмена вызова setInterval
-clearInterval(timerId2)
+// 2 пробег:
+// Идет выполнение кода построчно
 
-// Если первый аргумент является строкой, то JavaScript создаст из неё функцию (не рекомендуется)
-// setTimeout("console.log('Работает!')", 1000) // "Работает"
+// Каждый раз, когда вызывается функция, у нее создается новое лексическое окружение
+// Сборщик мусора не удаляет лексическое окружение до тех пор, пока на него есть хоть одна ссылка
 
-// Arguments:
-// handler - callback функция
-// timeout? - задержка в мили-секундах, перед запуском callback функции (0 по умолчанию)
-// ...arguments - аргументы, передаваемые в функцию
+const globalLexicalEnvironment = {
+  outer: null,
+  environmentRecord: {
+    a: 1, // undefined => 1
+    functionDeclaration: "ref",
+    result: "ref", // undefined => "ref"
+    b: 2
+  },
+}
+
+var a = 1
+let b = 2
+
+function functionDeclaration(arg) {
+  const functionDeclarationLexicalEnvironment = {
+    outer: globalLexicalEnvironment,
+    environmentRecord: {
+      showArg: "ref",
+      arg: 10, // undefined => 10
+    },
+  }
+
+  function showArg() {
+    const showArgLexicalEnvironment = { // Удаляется, т.к. ссылка на функцию showArg нигде не хранится
+      outer: functionDeclarationLexicalEnvironment,
+      environmentRecord: {},
+    }
+    const showArgLexicalEnvironment2 = { // Удаляется, т.к. ссылка на функцию showArg нигде не хранится
+      outer: functionDeclarationLexicalEnvironment,
+      environmentRecord: {},
+    }
+
+    console.log(arg)
+  }
+
+  return showArg
+}
+
+// В переменной result хранится ссылка на функцию functionDeclaration,
+// соответственно лексическое окружение functionDeclarationLexicalEnvironment не удаляется сборщиком мусора
+var result = functionDeclaration(10)
+
+result() // 10
+result() // 10
+
+// result = null // Сборщик мусора удаляет ссылку на функцию и лексическое окружения functionDeclarationLexicalEnvironment
+
+
+// Объект лексическое окружение не создает
+// const globalLexicalEnvironment = {
+//   outer: null,
+//   environmentRecord: {
+//     object: "ref"
+//   },
+// }
+//
+// const object = {
+//   name: "Evgeny",
+//   sayName() {
+//     const sayNameLexicalEnvironment = {
+//       outer: globalLexicalEnvironment,
+//       environmentRecord: {},
+//     }
+//
+//     console.log(this.name)
+//   }
+// }
+//
+// object.sayName() // "Evgeny"
+
+// Пример замыкания:
+// const sum = (a) => {
+//   return (b) => {
+//     return a + b
+//   }
+// }
+// const result = sum(3)(6)
+// console.log(result)
+
+// const makeCounter = () => {
+//   let count = 0
+//
+//   return () => {
+//     console.log(++count)
+//   }
+// }
+// const counter = makeCounter()
+// counter() // 1
+// counter() // 2
+//
+// const counter2 = makeCounter()
+// counter2() // 1
+// counter() // 3
