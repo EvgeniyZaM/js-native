5324b3e2f70b4db494ba0c4bb8bd04608c29b1f1
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-12-18 18:56:36 +0300

    commit

--- index.js
+++ index.js
@@ -1,71 +1,128 @@
 "use strict"
 
-// При использовании await, то только после того, как мы дождемся зарезолвленного promise-а, выполнится код, который после await.
-// При использовании await, когда мы не дождались когда promise зарезолвится, код который ниже не выполнится
-
-
 const promise = new Promise((resolve, reject) => {
-  // resolve("Data")
-  reject("Error")
+  resolve("Data")
+  // reject("Error")
 })
 
-// Цепочка promise-ов
-promise
-  // .catch((error)=>{
-  //   console.log(error) // "Error"
-  // })
-  .then((data) => {
-    console.log(data) // "Data"
-    return data.slice(1)
-  })
-  // .catch(() => {  // Для обработки ошибки двух верхних методов, catch и then
-  //   console.log("Error 1")
-  // })
-  .then((data) => {
-    console.log(data) // "ata"
-    return data.slice(1)
-  })
-  // .catch(() => {  // Для обработки ошибки двух верхних методов, catch и then
-  //   console.log("Error 2")
-  // })
-  .then((data) => {
-    console.log(data) // "ta"
-  })
+// promise
+//   .catch((error) => {
+//     console.log(error) // "Error"
+//   })
+//   .then((data) => {
+//     console.log(data) // "Data"
+//     return data.slice(1)
+//   })
+//   .catch(() => {  // Для обработки ошибки двух верхних методов, catch и then
+//     console.log("Error 1")
+//   })
+//   .then((data) => {
+//     console.log(data) // "ata"
+//     return data.slice(1)
+//   })
+//   .catch(() => {  // Для обработки ошибки двух верхних методов, catch и then
+//     console.log("Error 2")
+//   })
+//   .then((data) => {
+//     console.log(data) // "ta"
+//   })
 
-  // Срабатывает при вызове функции reject или когда в каком-то из методов then, в первом callback-е произошла ошибка
-  .catch(() => {
-    console.log("Error")
-    return "new error"
-  })
-  // .then(null, () => { // Альтернатива метода catch, которую лучше не использовать
-  //   console.log("Error")
-  //   return "new error"
-  // })
+// .catch(() => { // Срабатывает при вызове функции reject или когда в каком-то из методов then, в первом callback-е произошла ошибка
+//   console.log("Error")
+//   return "new error"
+// })
+// .then((data) => { // Первый callback метода then может принимать в качестве аргумента возвращенное значение в callback-е метода catch (catch не может)
+//   console.log(data) // "new error"
+// })
 
-  // Первый callback метода then может принимать в качестве аргумента возвращенное значение в callback-е метода catch (catch не может)
-  // Или возвращенное значение во втором callback-е метода then
-  .then((data) => {
-    console.log(data) // "new error"
-  })
-
+// Альтернативный подход, который использовать не нужно:
 // promise
 //   .then( // Первый then
 //     (data) => {
 //       console.log(data) // "Data"
-//       return "new data"
+//       return data.slice(1)
 //     },
-//     (error) => { // Вызывается если вызовем функцию reject в конструкторе promise
+//     (error) => { // Для обработки ошибки, при вызове функции reject
 //       console.log(error) // "Error"
 //     }
 //   )
 //   .then( // Второй then
-//     (data) => { // Возвращенное значение в первом then
-//       console.log(data) // "new data"
+//     (data) => {
+//       console.log(data) // "ata"
+//       return data.slice(1)
 //     },
-//     () => {  // Вызывается если ошибка будет в первом callback-е первого then
-//       console.log("Ошибка в первом callback-е первого then")
+//     () => { // Для обработки ошибки двух callback-ов первого метода then
+//       console.log("Error 1")
 //     }
 //   )
-//   .then(null, () => { // Вызывается если ошибка будет в первом callback-е второго then
-//     console.log("Ошибка во втором callback-е первого then")
-//   })
+//   .then(
+//     (data) => {
+//       console.log(data) // "ta"
+//     },
+//     () => { // Для обработки ошибки двух callback-ов второго метода then
+//       console.log("Error 2")
+//     }
+//   )
+//
+// .then(null, () => {   // Срабатывает при вызове функции reject или когда в каком-то из методов then, в первом callback-е произошла ошибка
+//   console.log("Error")
+//   return "my error"
+// })
+// .then((data) => { // Первый callback метода then может принимать в качестве аргумента возвращенное значение во втором callback-е метода then
+//   console.log(data) // "my error"
+// })
+
+// В callback-е метода finally происходит ошибка и он удаляет всю дальнейшую цепочку promise-ов
+// .then((data) => { // Отработает
+//   console.log(data) // "Data"
+//   return data
+// })
+// .finally(() => {
+//   throw new Error() // Происходит ошибка в callback-е
+// })
+// .then((data) => { // Не отработает
+//   console.log(data)
+//   return data
+// })
+// .then((data) => { // Не отработает
+//   console.log(data)
+// })
+// .catch(() => { // Перехватит ошибку, которая была в callback-е метода finally
+//   console.log("Error") // "Error"
+// })
+
+// const promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Data 1")
+//   }, 1000)
+// })
+//
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Data 2")
+//   }, 2000)
+// })
+//
+// const promise4 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("Data 3")
+//   }, 500)
+// })
+
+// const resolve = (Promise.resolve("Date"))
+// console.log(resolve)
+
+// const reject = Promise.reject("Error")
+// console.log(reject)
+
+// const any = Promise.any([promise2, promise3, promise4])
+// console.log(any)
+
+// const race = Promise.race([promise2, promise3, promise4])
+// console.log(race)
+
+// const allSettled = Promise.allSettled([promise2, promise3, promise4])
+// console.log(allSettled)
+
+// const all = Promise.all([promise2, promise3, promise4])
+// console.log(all)
