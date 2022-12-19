98d396ce7688f005fab4f9d9cea587f1e18141aa
EvgeniyZamatevskiy <jerry.zam.999@gmail.com>
2022-12-19 12:31:04 +0300

    commit

--- index.js
+++ index.js
@@ -2,37 +2,33 @@
 
 const promise = new Promise((resolve, reject) => {
   resolve("Data")
-  // reject("Error")
+  //reject("Error")
 })
 
-// promise
-//   .catch((error) => {
-//     console.log(error) // "Error"
-//   })
-//   .then((data) => {
-//     console.log(data) // "Data"
-//     return data.slice(1)
-//   })
-//   .catch(() => {  // Для обработки ошибки двух верхних методов, catch и then
-//     console.log("Error 1")
-//   })
-//   .then((data) => {
-//     console.log(data) // "ata"
-//     return data.slice(1)
-//   })
-//   .catch(() => {  // Для обработки ошибки двух верхних методов, catch и then
-//     console.log("Error 2")
-//   })
-//   .then((data) => {
-//     console.log(data) // "ta"
-//   })
-
-// .catch(() => { // Срабатывает при вызове функции reject или когда в каком-то из методов then, в первом callback-е произошла ошибка
-//   console.log("Error")
-//   return "new error"
-// })
-// .then((data) => { // Первый callback метода then может принимать в качестве аргумента возвращенное значение в callback-е метода catch (catch не может)
-//   console.log(data) // "new error"
+promise
+  .catch((error) => {
+    console.log(error)
+  })
+  .then((data) => {
+    console.log(data) // "Data"
+    return data.slice(1)
+  })
+  .catch((error) => {  // Для обработки ошибки двух верхних методов, catch и then
+    console.log(error.message)
+  })
+  .then((data) => {
+    console.log(data) // "ata"
+    return data.slice(1)
+  })
+  .catch((error) => {  // Для обработки ошибки двух верхних методов, catch и then
+    console.log(error.message)
+  })
+  .then((data) => {
+    console.log(data) // "ta"
+  })
+// // нижний catch, который срабатывает когда любой promise зареджектится
+// .catch((error) => {
+//   console.log(error)
 // })
 
 // Альтернативный подход, который использовать не нужно:
@@ -43,7 +39,7 @@ const promise = new Promise((resolve, reject) => {
 //       return data.slice(1)
 //     },
 //     (error) => { // Для обработки ошибки, при вызове функции reject
-//       console.log(error) // "Error"
+//       console.log(error)
 //     }
 //   )
 //   .then( // Второй then
@@ -51,26 +47,22 @@ const promise = new Promise((resolve, reject) => {
 //       console.log(data) // "ata"
 //       return data.slice(1)
 //     },
-//     () => { // Для обработки ошибки двух callback-ов первого метода then
-//       console.log("Error 1")
+//     (error) => { // Для обработки ошибки двух callback-ов первого метода then
+//       console.log(error.message)
 //     }
 //   )
 //   .then(
 //     (data) => {
 //       console.log(data) // "ta"
 //     },
-//     () => { // Для обработки ошибки двух callback-ов второго метода then
-//       console.log("Error 2")
+//     (error) => { // Для обработки ошибки двух callback-ов второго метода then
+//       console.log(error.message)
 //     }
 //   )
-//
-// .then(null, () => {   // Срабатывает при вызове функции reject или когда в каком-то из методов then, в первом callback-е произошла ошибка
-//   console.log("Error")
-//   return "my error"
-// })
-// .then((data) => { // Первый callback метода then может принимать в качестве аргумента возвращенное значение во втором callback-е метода then
-//   console.log(data) // "my error"
-// })
+// // нижний then, который срабатывает когда любой promise зареджектится
+// // .then(null, (error) => {
+// //   console.log(error)
+// // })
 
 // В callback-е метода finally происходит ошибка и он удаляет всю дальнейшую цепочку promise-ов
 // .then((data) => { // Отработает
@@ -78,7 +70,7 @@ const promise = new Promise((resolve, reject) => {
 //   return data
 // })
 // .finally(() => {
-//   throw new Error() // Происходит ошибка в callback-е
+//   throw new Error("my error") // Происходит ошибка в callback-е
 // })
 // .then((data) => { // Не отработает
 //   console.log(data)
@@ -87,42 +79,50 @@ const promise = new Promise((resolve, reject) => {
 // .then((data) => { // Не отработает
 //   console.log(data)
 // })
-// .catch(() => { // Перехватит ошибку, которая была в callback-е метода finally
-//   console.log("Error") // "Error"
+// .catch((error) => { // Перехватит ошибку, которая была в callback-е метода finally
+//   console.log(error.message) // "my error"
 // })
 
-// const promise2 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("Data 1")
-//   }, 1000)
-// })
-//
-// const promise3 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("Data 2")
-//   }, 2000)
-// })
-//
-// const promise4 = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("Data 3")
-//   }, 500)
-// })
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data 1")
+  }, 1000)
+})
 
-// const resolve = (Promise.resolve("Date"))
-// console.log(resolve)
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data 2")
+  }, 2000)
+})
 
-// const reject = Promise.reject("Error")
-// console.log(reject)
-
-// const any = Promise.any([promise2, promise3, promise4])
-// console.log(any)
-
-// const race = Promise.race([promise2, promise3, promise4])
-// console.log(race)
-
-// const allSettled = Promise.allSettled([promise2, promise3, promise4])
-// console.log(allSettled)
+const promise4 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data 3")
+  }, 500)
+})
 
 // const all = Promise.all([promise2, promise3, promise4])
 // console.log(all)
+// // [[PromiseState]]: "fulfilled"
+// // [[PromiseResult]]: Array(3)
+
+// const race = Promise.race([promise2, promise3, promise4])
+// console.log(race)
+// // [[PromiseState]]: "fulfilled"
+// // [[PromiseResult]]: "Data 3"
+
+// const any = Promise.any([promise2, promise3, promise4])
+// console.log(any)
+// // [[PromiseState]]: "fulfilled"
+// // [[PromiseResult]]: "Data 3"
+
+// const allSettled = Promise.allSettled([promise2, promise3, promise4])
+// console.log(allSettled)
+// // [[PromiseState]]: "fulfilled"
+// // [[PromiseResult]]: Array(3)
+
+// const resolve = (Promise.resolve("Date"))
+// console.log(resolve) // Promise{<fulfilled>: 'Date'}
+
+// const reject = Promise.reject("Error")
+// console.log(reject) // Promise{<rejected>: 'Error'}
